ff2b68122012e8b84ff7042869117f3e047f9998
kuaqi <kuaqi@users.noreply.github.com>
2020-02-23 15:15:03 +0800

    Remove AsyncTaskLoader and NetworkUtil implementations

--- app/src/main/java/com/whip/analyticsdashboard/MainActivity.java
+++ app/src/main/java/com/whip/analyticsdashboard/MainActivity.java
@@ -1,30 +1,18 @@
 package com.whip.analyticsdashboard;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.whip.analyticsdashboard.adapter.PieAdapter;
 import com.whip.analyticsdashboard.adapter.PieChartAdapter;
 import com.whip.analyticsdashboard.model.AnalyticsData;
-import com.whip.analyticsdashboard.model.Pie;
-import com.whip.analyticsdashboard.network.JSONParser;
 import com.whip.analyticsdashboard.model.PieChart;
 import com.whip.analyticsdashboard.network.Retrofit.AnalyticsService;
 import com.whip.analyticsdashboard.network.Retrofit.RetrofitClient;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -33,19 +21,16 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     AnalyticsService analyticsService;
     CompositeDisposable compositeDisposable;
-    ArrayList<Pie> pieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        pieList = new ArrayList<>();
 
 //        getMockDashboard();
 
@@ -83,42 +68,5 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onStop() {
         compositeDisposable.clear();
         super.onStop();
-    }
-
-    public void getMockDashboard() {
-        ConnectivityManager connectivityManager =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = null;
-        if (connectivityManager != null)
-            networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-            getSupportLoaderManager().restartLoader(0, null, this);
-        } else {
-            Toast.makeText(getApplicationContext(), "Internet not connected.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @NonNull
-    @Override
-    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
-        String queryString = "LAST_7_DAYS";
-        return new JobLoader(this, queryString);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
-        Log.d(LOG_TAG, "onLoadFinished");
-        pieList = JSONParser.parseOperationsResponse(data);
-        setPieChartView();
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<String> loader) { }
-
-    public void setPieChartView() {
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_pie);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        PieAdapter adapter = new PieAdapter(this, pieList);
-        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/whip/analyticsdashboard/NetworkUtil.java
+++ app/src/main/java/com/whip/analyticsdashboard/NetworkUtil.java
@@ -1,72 +1,11 @@
 package com.whip.analyticsdashboard;
 
-import android.net.Uri;
 import android.util.Log;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class NetworkUtil {
     public static final String LOG_TAG = NetworkUtil.class.getSimpleName();
 
     public static final String BASE_URL= "https://skyrim.whipmobility.io/v10/analytic/dashboard/operation/";
     public static final String QUERY_PARAM = "scope";
-
-    static String getJobInfo(String queryString) {
-        HttpURLConnection urlConnection = null;     // To connect to the internet
-        BufferedReader reader = null;               // To read incoming data
-        String jobJSONString = null;                // To store the JSON response
-
-        try {
-            Uri builtURI = Uri.parse(BASE_URL).buildUpon()
-                    .appendQueryParameter(QUERY_PARAM, queryString)
-                    .build();
-
-            URL requestURL = new URL(builtURI.toString());
-            Log.d(LOG_TAG, "Full query URL: " + builtURI.toString());
-            urlConnection = (HttpURLConnection) requestURL.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder builder = new StringBuilder();
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
-                builder.append("\n");   // For log debugging purpose
-            }
-
-            if (builder.length() == 0)
-                return null;
-
-            Log.d(LOG_TAG, "getJobInfo - SUCCESS");
-            jobJSONString = builder.toString();
-
-        } catch (IOException e) {
-            Log.d(LOG_TAG, "getJobInfo - ERROR");
-            e.printStackTrace();
-
-        } finally {
-            if (urlConnection != null)
-                urlConnection.disconnect();
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch(IOException e) {
-                    Log.d(LOG_TAG, "getJobInfo - ERROR");
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        Log.d(LOG_TAG, jobJSONString);
-        return jobJSONString;
-    }
 
 }
